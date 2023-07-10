deaf2c564fe5e23bd038bf9b0dd9efe571a69f98
koray öngün <koraygg28@gmail.com>
2023-07-10 09:26:42 +0300

    .

--- dijikstra.py
+++ dijikstra.py
@@ -1,4 +1,4 @@
-lines = [["a","e",1],["a","c",6],["a","b",5],["e","c",2],["c","b",1],["c","d",4],["b","f",1],["f","d",3],["f","h",2],["d","h",5]]
+lines = [["a","c",6],["a","b",5],["e","c",2],["c","b",1],["b","f",1],["c","d",4],["f","d",3],["d","h",5],["f","h",2],["a","e",1]]
 
 start = "a"
 target = "h"
@@ -12,12 +12,15 @@ distance = {
     "f":float("inf"),
     "h":float("inf")}
 
-for s,d,p in lines:
-    if s==start:
-        distance[d]=p
-        continue
-    else:
-        if distance[s]+p < distance[d]:
-            distance[d] = distance[s]+p
+for i in range(2):
+    for s,d,p in lines:
+        if s==start and distance[d]==float("inf"):
+            distance[d]=p
+            continue
+        else:
+            if distance[s]+p < distance[d]:
+                distance[d] = distance[s]+p
+        
+    
 
 print(distance)
